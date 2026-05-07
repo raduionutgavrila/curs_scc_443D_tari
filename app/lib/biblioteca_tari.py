6b05427533e4af6a956f0cbcfdc4fdd8cd5874f5
DaWall <ziducristian@yahoo.com>
2026-05-07 23:46:38 +0300

    Estonia in biblioteca_tari

--- app/lib/biblioteca_tari.py
+++ app/lib/biblioteca_tari.py
@@ -1,11 +1,11 @@
 # Configuratie globala a proiectului
 
 #from app.lib import biblioteca_<tara_mea> as prescurtare_tara_mea 
-from app.lib import biblioteca_belgia as belg
+from app.lib import biblioteca_estonia as esto
 
 TARI = {
-    'belgia': {
-        'nume': 'Belgia',
+    'estonia': {
+        'nume': 'Estonia',
     },
     #adauga  'tara_mea': { 'nume': 'Nume Tara Mea'}
 
@@ -15,7 +15,7 @@ TARI = {
 # Mapare tara -> biblioteca
 
 BIBLIOTECI = {
-    'belgia': belg,
+    'estonia': esto,
      #adauga 'tara_mea': prescurtare_tara_mea
 }
 
