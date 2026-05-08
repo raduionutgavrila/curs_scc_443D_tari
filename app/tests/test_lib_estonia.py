d423ed35731d277e079822f5ced96ee3de2dd061
DaWall <ziducristian@yahoo.com>
2026-05-08 19:10:43 +0300

    S-a adaugat test_lib_estonia

---
+++ app/tests/test_lib_estonia.py
@@ -0,0 +1,40 @@
+import logging
+from app.lib.biblioteca_belgia import descriere_tara, descriere_capitala, descriere_limbi, descriere_populatie
+
+#from app.lib.biblioteca_<tara_mea> import descriere_tara, descriere_capitala, descriere_limbi, descriere_populatie
+
+logger = logging.getLogger(__name__)
+
+
+def test_functie_descriere_tara():
+    expected_result = "Estonia se află la Marea Baltică, având câmpii joase, păduri dense, mii de lacuri, întinse zone umede și o coastă cu peste două mii de insule glaciale."
+    result = descriere_tara()
+    assert result == expected_result, f"Test eșuat! Rezultatul funcției descriere_tara() este: {result}"
+    logger.info("Merge functia descriere_tara")
+
+def test_functie_populatie():
+    expected_result = "1.36 milioane de locuitori"
+    result = descriere_populatie()
+    assert result == expected_result, f"Test eșuat! Rezultatul funcției descriere_populatie() este: {result}"
+    logger.info("Merge functia descriere_populatie")
+
+
+def test_functie_capitala():
+    expected_result = "Tallinn"
+    result = descriere_capitala()
+    assert result == expected_result, f"Test eșuat! Rezultatul funcției descriere_capitala() este: {result}"
+    logger.info("Merge functia descriere_capitala")
+
+
+def test_functie_limbi():
+    expected_result = "Estona"
+    result = descriere_limbi()
+    assert result == expected_result, f"Test eșuat! Rezultatul funcției descriere_limbi() este: {result}"
+    logger.info("Merge functia descriere_limbi")
+
+
+if __name__ == "__main__":
+    test_functie_descriere_tara()
+    test_functie_populatie()
+    test_functie_capitala()
+    test_functie_limbi()
