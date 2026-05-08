54f1ca8d7735f0e9d28ba44ecbca48496540d1f1
razwanel <costin.gheorghe2205@stud.etti.upb.ro>
2026-05-08 17:52:41 +0300

    app\tests

--- app/tests/test_lib_romania.py
+++ app/tests/test_lib_romania.py
@@ -7,27 +7,27 @@ logger = logging.getLogger(__name__)
 
 
 def test_functie_descriere_tara():
-    expected_result = "Regatul Belgiei este un stat federal suveran situat în Europa de Vest, caracterizat printr-o structură administrativă tripartită și un model de guvernanță multilingv, exercitând rolul de hub geopolitic major prin găzduirea principalelor instituții ale Uniunii Europene și ale NATO."
+    expected_result = "România este o țară din Europa de Sud-Est, cu o istorie bogată, cultură diversă și peisaje variate, inclusiv Munții Carpați, câmpii fertile și litoral la Marea Neagră."
     result = descriere_tara()
     assert result == expected_result, f"Test eșuat! Rezultatul funcției descriere_tara() este: {result}"
     logger.info("Merge functia descriere_tara")
 
 def test_functie_populatie():
-    expected_result = "11.5 milioane de locuitori"
+    expected_result = "18.8 milioane de locuitori"
     result = descriere_populatie()
     assert result == expected_result, f"Test eșuat! Rezultatul funcției descriere_populatie() este: {result}"
     logger.info("Merge functia descriere_populatie")
 
 
 def test_functie_capitala():
-    expected_result = "Bruxelles"
+    expected_result = "București"
     result = descriere_capitala()
     assert result == expected_result, f"Test eșuat! Rezultatul funcției descriere_capitala() este: {result}"
     logger.info("Merge functia descriere_capitala")
 
 
 def test_functie_limbi():
-    expected_result = "Neerlandeza, Franceza, Germana"
+    expected_result = "Română"
     result = descriere_limbi()
     assert result == expected_result, f"Test eșuat! Rezultatul funcției descriere_limbi() este: {result}"
     logger.info("Merge functia descriere_limbi")
