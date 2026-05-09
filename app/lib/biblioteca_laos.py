71468255741e58635b2108d669bf61030c483175
Cucui Mihai <mihaicucui123@gmail.com>
2026-05-10 02:42:51 +0300

    Salvare intermediara Laos,teste initiale trecute

---
+++ app/lib/biblioteca_laos.py
@@ -0,0 +1,14 @@
+def descriere_tara() -> str:
+    return "Laos este o republica socialista din Asia de Sud-Est, cunoscuta pentru peisajele sale montane si fluviul Mekong."
+
+def descriere_limbi() -> str:
+    return "Lao"
+
+def descriere_populatie() -> str:
+    return "7.5 milioane de locuitori"
+
+def descriere_capitala() -> str:
+    return "Vientiane"
+
+def descriere_steag() -> str:
+    return """<img src="/static/steag_laos.png" alt="Drapel Laos">"""
--- app/tests/test_lib_laos.py
+++ app/tests/test_lib_laos.py
@@ -1,5 +1,5 @@
 import logging
-from app.lib.biblioteca_belgia import descriere_tara, descriere_capitala, descriere_limbi, descriere_populatie
+from app.lib.biblioteca_laos import descriere_tara, descriere_capitala, descriere_limbi, descriere_populatie
 
 #from app.lib.biblioteca_<tara_mea> import descriere_tara, descriere_capitala, descriere_limbi, descriere_populatie
 
@@ -7,27 +7,27 @@ logger = logging.getLogger(__name__)
 
 
 def test_functie_descriere_tara():
-    expected_result = "Regatul Belgiei este un stat federal suveran situat în Europa de Vest, caracterizat printr-o structură administrativă tripartită și un model de guvernanță multilingv, exercitând rolul de hub geopolitic major prin găzduirea principalelor instituții ale Uniunii Europene și ale NATO."
+    expected_result = "Laos este o republica socialista din Asia de Sud-Est, cunoscuta pentru peisajele sale montane si fluviul Mekong."
     result = descriere_tara()
     assert result == expected_result, f"Test eșuat! Rezultatul funcției descriere_tara() este: {result}"
     logger.info("Merge functia descriere_tara")
 
 def test_functie_populatie():
-    expected_result = "11.5 milioane de locuitori"
+    expected_result = "7.5 milioane de locuitori"
     result = descriere_populatie()
     assert result == expected_result, f"Test eșuat! Rezultatul funcției descriere_populatie() este: {result}"
     logger.info("Merge functia descriere_populatie")
 
 
 def test_functie_capitala():
-    expected_result = "Bruxelles"
+    expected_result = "Vientiane"
     result = descriere_capitala()
     assert result == expected_result, f"Test eșuat! Rezultatul funcției descriere_capitala() este: {result}"
     logger.info("Merge functia descriere_capitala")
 
 
 def test_functie_limbi():
-    expected_result = "Neerlandeza, Franceza, Germana"
+    expected_result = "Lao"
     result = descriere_limbi()
     assert result == expected_result, f"Test eșuat! Rezultatul funcției descriere_limbi() este: {result}"
     logger.info("Merge functia descriere_limbi")
