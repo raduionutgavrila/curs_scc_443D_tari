5d171d051bceb0a0efc47381b948333111333f63
Cucui Petrut <petrutcucui@gmail.com>
2026-05-10 02:41:27 +0300

    teste trecute

--- app/lib/biblioteca_tari.py
+++ app/lib/biblioteca_tari.py
@@ -1,22 +1,19 @@
-# Configuratie globala a proiectului
 
-#from app.lib import biblioteca_<tara_mea> as prescurtare_tara_mea 
-from app.lib import biblioteca_belgia as belg
+ 
+from app.lib import biblioteca_nepal as nepa
 
 TARI = {
-    'belgia': {
-        'nume': 'Belgia',
+    'nepal': {
+        'nume': 'Nepal',
     },
-    #adauga  'tara_mea': { 'nume': 'Nume Tara Mea'}
 
 }
 
 
-# Mapare tara -> biblioteca
+
 
 BIBLIOTECI = {
-    'belgia': belg,
-     #adauga 'tara_mea': prescurtare_tara_mea
+    'nepal': nepa, 
 }
 
 
--- app/tests/test_lib_nepal.py
+++ app/tests/test_lib_nepal.py
@@ -1,5 +1,5 @@
 import logging
-from app.lib.biblioteca_belgia import descriere_tara, descriere_capitala, descriere_limbi, descriere_populatie
+from app.lib.biblioteca_nepal import descriere_tara, descriere_capitala, descriere_limbi, descriere_populatie
 
 #from app.lib.biblioteca_<tara_mea> import descriere_tara, descriere_capitala, descriere_limbi, descriere_populatie
 
@@ -7,27 +7,27 @@ logger = logging.getLogger(__name__)
 
 
 def test_functie_descriere_tara():
-    expected_result = "Regatul Belgiei este un stat federal suveran situat în Europa de Vest, caracterizat printr-o structură administrativă tripartită și un model de guvernanță multilingv, exercitând rolul de hub geopolitic major prin găzduirea principalelor instituții ale Uniunii Europene și ale NATO."
+    expected_result = "Republica Federala Democratica Nepal este un stat muntos situat in Asia de Sud, organizat ca republica parlamentara federala. Tara este impartita in 7 provincii fiind un centru cultural si spiritual major in regiunea Himalaya."
     result = descriere_tara()
     assert result == expected_result, f"Test eșuat! Rezultatul funcției descriere_tara() este: {result}"
     logger.info("Merge functia descriere_tara")
 
 def test_functie_populatie():
-    expected_result = "11.5 milioane de locuitori"
+    expected_result = "30.5 milioane de locuitori"
     result = descriere_populatie()
     assert result == expected_result, f"Test eșuat! Rezultatul funcției descriere_populatie() este: {result}"
     logger.info("Merge functia descriere_populatie")
 
 
 def test_functie_capitala():
-    expected_result = "Bruxelles"
+    expected_result = "Kathmandu"
     result = descriere_capitala()
     assert result == expected_result, f"Test eșuat! Rezultatul funcției descriere_capitala() este: {result}"
     logger.info("Merge functia descriere_capitala")
 
 
 def test_functie_limbi():
-    expected_result = "Neerlandeza, Franceza, Germana"
+    expected_result = "Nepaleza, Maithili, Bhojpuri, Tharu, Tamang."
     result = descriere_limbi()
     assert result == expected_result, f"Test eșuat! Rezultatul funcției descriere_limbi() este: {result}"
     logger.info("Merge functia descriere_limbi")
