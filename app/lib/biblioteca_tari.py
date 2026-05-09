fbe1aeed80c6f97718f669b344e8be0d86cca242
andrei <andrei.ciobanu031@gmail.com>
2026-05-09 15:11:18 +0300

    Adaugat tara Japonia

--- app/lib/biblioteca_tari.py
+++ app/lib/biblioteca_tari.py
@@ -1,11 +1,11 @@
 # Configuratie globala a proiectului
 
 #from app.lib import biblioteca_<tara_mea> as prescurtare_tara_mea 
-from app.lib import biblioteca_belgia as belg
+from app.lib import biblioteca_japonia as jp
 
 TARI = {
-    'belgia': {
-        'nume': 'Belgia',
+    "japonia": {
+        "nume": "Japonia",
     },
     #adauga  'tara_mea': { 'nume': 'Nume Tara Mea'}
 
@@ -15,7 +15,7 @@ TARI = {
 # Mapare tara -> biblioteca
 
 BIBLIOTECI = {
-    'belgia': belg,
+    "japonia": jp,
      #adauga 'tara_mea': prescurtare_tara_mea
 }
 
