483fc4acc908be3cccf6357e9c9dda15e720e557
marapirjol <marapirjol17@gmail.com>
2026-05-09 18:22:50 +0300

    Proiect Irlanda

---
+++ dockerstart.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "Activare venv:"
+#source .vanv/bin/activate
+. .venv/bin/activate
+echo "Configurare variabila mediu FLASK_APP"
+export FLASK_APP=tari
+cale=`pwd`
+echo "CALE: " $x
+lsdir=`ls -1`
+echo "CONTINUT DIRECTOR: " $lsdir
+echo "Start server:"
+exec flask run -h 0.0.0.0 -p 5011 --reload
