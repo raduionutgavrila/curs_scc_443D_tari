143945f1f9469fb8b5d31b04a0f11f20158b0b9b
etuzorr <etuzorr@gmail.com>
2026-05-09 14:52:48 +0300

    Adauga Jenkinsfile si Dockerfile

--- app/lib/biblioteca_tari.py
+++ app/lib/biblioteca_tari.py
@@ -1,11 +1,11 @@
 # Configuratie globala a proiectului
 
 #from app.lib import biblioteca_<tara_mea> as prescurtare_tara_mea 
-from app.lib import biblioteca_belgia as belg
+from app.lib import biblioteca_statele_unite as sua
 
 TARI = {
-    'belgia': {
-        'nume': 'Belgia',
+    'sua': {
+        'nume': 'Statele Unite ale Americii',
     },
     #adauga  'tara_mea': { 'nume': 'Nume Tara Mea'}
 
@@ -15,7 +15,7 @@ TARI = {
 # Mapare tara -> biblioteca
 
 BIBLIOTECI = {
-    'belgia': belg,
+    'sua': sua,
      #adauga 'tara_mea': prescurtare_tara_mea
 }
 
