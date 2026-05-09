3cecda5e8d082a29cc1620284cff3a4a86710120
BogdanE <bogdan.en03@gmail.com>
2026-05-09 19:15:17 +0300

    Added Norway Implementation

--- app/lib/biblioteca_tari.py
+++ app/lib/biblioteca_tari.py
@@ -1,11 +1,11 @@
 # Configuratie globala a proiectului
 
 #from app.lib import biblioteca_<tara_mea> as prescurtare_tara_mea 
-from app.lib import biblioteca_belgia as belg
+from app.lib import biblioteca_norvegia as nvg
 
 TARI = {
-    'belgia': {
-        'nume': 'Belgia',
+    'norvegia': {
+        'nume': 'Norvegia',
     },
     #adauga  'tara_mea': { 'nume': 'Nume Tara Mea'}
 
@@ -15,7 +15,7 @@ TARI = {
 # Mapare tara -> biblioteca
 
 BIBLIOTECI = {
-    'belgia': belg,
+    'norvegia': nvg,
      #adauga 'tara_mea': prescurtare_tara_mea
 }
 
--- app/tests/test_lib_norvegia.py
+++ app/tests/test_lib_norvegia.py
@@ -1,5 +1,5 @@
 import logging
-from app.lib.biblioteca_belgia import descriere_tara, descriere_capitala, descriere_limbi, descriere_populatie
+from app.lib.biblioteca_norvegia import descriere_tara, descriere_capitala, descriere_limbi, descriere_populatie
 
 #from app.lib.biblioteca_<tara_mea> import descriere_tara, descriere_capitala, descriere_limbi, descriere_populatie
 
@@ -7,27 +7,27 @@ logger = logging.getLogger(__name__)
 
 
 def test_functie_descriere_tara():
-    expected_result = "Regatul Belgiei este un stat federal suveran situat în Europa de Vest, caracterizat printr-o structură administrativă tripartită și un model de guvernanță multilingv, exercitând rolul de hub geopolitic major prin găzduirea principalelor instituții ale Uniunii Europene și ale NATO."
+    expected_result = "Regatul Norvegiei este un stat suveran și o monarhie constituțională situată în Europa de Nord, pe Peninsula Scandinavă, renumit pentru peisajele sale spectaculoase cu fiorduri. Este un stat non-UE, dar face parte din Spațiul Economic European (SEE) și este membru fondator al NATO."
     result = descriere_tara()
     assert result == expected_result, f"Test eșuat! Rezultatul funcției descriere_tara() este: {result}"
     logger.info("Merge functia descriere_tara")
 
 def test_functie_populatie():
-    expected_result = "11.5 milioane de locuitori"
+    expected_result = "5.572 milioane de locuitori"
     result = descriere_populatie()
     assert result == expected_result, f"Test eșuat! Rezultatul funcției descriere_populatie() este: {result}"
     logger.info("Merge functia descriere_populatie")
 
 
 def test_functie_capitala():
-    expected_result = "Bruxelles"
+    expected_result = "Oslo"
     result = descriere_capitala()
     assert result == expected_result, f"Test eșuat! Rezultatul funcției descriere_capitala() este: {result}"
     logger.info("Merge functia descriere_capitala")
 
 
 def test_functie_limbi():
-    expected_result = "Neerlandeza, Franceza, Germana"
+    expected_result = "Norvegiana (Bokmal si Nynorsk), Sami"
     result = descriere_limbi()
     assert result == expected_result, f"Test eșuat! Rezultatul funcției descriere_limbi() este: {result}"
     logger.info("Merge functia descriere_limbi")
