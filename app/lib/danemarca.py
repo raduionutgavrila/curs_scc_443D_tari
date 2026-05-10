aaae905465387cd003067eacbd43c3335b7b2b5f
shdluca9 <lucas23.ivan@gmail.com>
2026-05-11 01:07:54 +0300

    Finalizare structura biblioteci, port 5011 si README

--- app/lib/danemarca.py
+++
@@ -1,5 +0,0 @@
-def descriere_danemarca():
-    return "Danemarca este o tara nordica, cunoscuta pentru designul sau, biciclete si conceptul de 'hygge'."
-
-def capitala_danemarca():
-    return "Capitala Danemarcei este Copenhaga, un oras vibrant si plin de istorie."
--- tari.py
+++ tari.py
@@ -1,33 +1,47 @@
 from flask import Flask, render_template
+from app.lib.biblioteca_tari import TARI, BIBLIOTECI
+from app.lib import biblioteca_header as header
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    lista_tari = {
-        "danemarca": {
-            "nume": "Danemarca"
-        }
-    }
-    return render_template('home.html', tari=lista_tari)
+    return render_template('home.html', tari=TARI)
 
 @app.route('/danemarca')
 def danemarca_home():
-    descriere = "Danemarca este o țară situată în Europa de Nord, recunoscută pentru calitatea ridicată a vieții, peisajele sale pitorești și cultura bicicletelor."
-    return render_template('tara.html', tara_nume="Danemarca", tara="danemarca", descriere=descriere, tara_url="/danemarca")
+    tara = BIBLIOTECI['danemarca']
+    return render_template('tara.html', 
+                           tara_nume="Danemarca", 
+                           tara="danemarca", 
+                           descriere=tara.descriere_tara(), 
+                           tara_url="/danemarca")
 
 @app.route('/danemarca/capitala')
 def feature_capitala():
-    return render_template('pagina.html', tara_nume="Danemarca", tara="danemarca", titlu="Capitala", continut="Capitala Danemarcei este Copenhaga.", tara_url="/danemarca")
+    tara = BIBLIOTECI['danemarca']
+    return render_template('pagina.html', 
+                           tara_nume="Danemarca", 
+                           titlu=header.header_capitala(), 
+                           continut=tara.descriere_capitala(), 
+                           tara_url="/danemarca")
 
 @app.route('/danemarca/steag')
 def feature_steag():
-    cod_html_steag = "<img src='/static/steag_danemarca.png' alt='Steag Danemarca' style='max-width: 500px;'>"
-    return render_template('steag.html', tara_url="/danemarca", header="Steagul Danemarcei", continut=cod_html_steag)
+    tara = BIBLIOTECI['danemarca']
+    return render_template('steag.html', 
+                           header=header.header_steag(), 
+                           continut=tara.descriere_steag(), 
+                           tara_url="/danemarca")
 
 @app.route('/danemarca/populatie')
 def feature_populatie():
-    return render_template('pagina.html', tara_nume="Danemarca", tara="danemarca", titlu="Populație", continut="Populația Danemarcei este de aproximativ 5.9 milioane de locuitori.", tara_url="/danemarca")
+    tara = BIBLIOTECI['danemarca']
+    return render_template('pagina.html', 
+                           tara_nume="Danemarca", 
+                           titlu=header.header_populatie(), 
+                           continut=tara.descriere_populatie(), 
+                           tara_url="/danemarca")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5011)
