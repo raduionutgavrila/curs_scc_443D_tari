fa520184173dc112203af3b490b5294aa51a88df
raduionutgavrila <153100829+raduionutgavrila@users.noreply.github.com>
2026-05-11 02:56:55 +0300

    Update dockerstart.sh

--- dockerstart.sh
+++ dockerstart.sh
@@ -9,16 +9,4 @@ echo "CALE: " $x
 lsdir=`ls -1`
 echo "CONTINUT DIRECTOR: " $lsdir
 echo "Start server:"
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 exec flask run -h 0.0.0.0 -p 5011 --reload
-=======
-exec flask run -h 0.0.0.0 -p 5011 --reload
->>>>>>> origin/main_pirjol_mara
-=======
-exec flask run -h 0.0.0.0 -p 5011 --reload
->>>>>>> origin/main_ivan_luca
-=======
-exec flask run -h 0.0.0.0 -p 5011 --reload
->>>>>>> origin/main_voicu_ioan_andrei
