2d3d6185c6cb7912385f29df49aa76f4fad6883a
shdluca9 <lucas23.ivan@gmail.com>
2026-05-11 01:21:52 +0300

    Aliniere stilistica biblioteca_tari cu template

--- app/lib/biblioteca_tari.py
+++ app/lib/biblioteca_tari.py
@@ -1,10 +1,16 @@
+# Configuratie globala a proiectului
+
+#from app.lib import biblioteca_<tara_mea> as prescurtare_tara_mea
 from app.lib import biblioteca_danemarca as dan
 
 TARI = {
     'danemarca': {
-        'nume': 'Danemarca'
+        'nume': 'Danemarca',
     }
+
 }
+
+# Mapare tara -> biblioteca
 
 BIBLIOTECI = {
     'danemarca': dan
